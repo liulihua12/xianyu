e331e59f7a4039e77dd1cad043f2068e9d2bed86
original dream <38641552+liulihua12@users.noreply.github.com>
2019-06-15 00:13:06 +0800

    Add files via upload

---
+++ src/components/commou/Bofang1.vue
@@ -0,0 +1,216 @@
+<template>
+    <div>
+        <div class="vdo-title">
+            <p title="减肥食谱一日三餐如何安排？">减肥食谱一日三餐如何安排？</p>
+            <span>517082人看了这个视频</span>
+        </div>
+        <div class="vdo-div">
+            <div class="vdo-vdo">
+                <video id="v1" :src="src" loop muted autoplay controls width="500" height="300"></video>
+                <span>17条相关视频</span>
+                <div class="vdo-ul">
+                    <ul v-for="(item,i) of list" ref="ul" :key="i">
+                        <li class="vdo-li" @click="play(item.src)" :src="item.src">
+                            <div>
+                                <div class="vdo-img">
+                                    <img :src="item.img" alt="">
+                                </div>
+                                <img src="../../assets/play_list_icon.png" alt="">
+                            </div>
+                            <div class="vdo-p-div">
+                                <p>{{item.title}}</p>
+                                <div class="vdo-p-img">
+                                    <p></p>
+                                    <span>{{item.span}}</span>
+                                </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <ul class="ul-img">
+                <li><img src="../../assets/1.jpg" alt=""><span>1</span></li>
+                <li><img src="../../assets/2.jpg" alt=""><span>2</span></li>
+                <li><img src="../../assets/3.jpg" alt=""><span>3</span></li>
+                <li><img src="../../assets/4.jpg" alt=""><span>4</span></li>
+            </ul>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        data(){
+            return {
+                src:"video-0.mp4",
+                list:[
+                    {vid:1,img:"./img/video-2.jpg",src:"video-0.mp4",title:"一日三餐减肥食谱",span:"百姓健康网"},
+                    {vid:2,img:"./img/video-1.png",src:"video-1.mp4",title:"二日三餐减肥食谱",span:"百姓健康网"},
+                    {vid:3,img:"./img/video-2.jpg",src:"video-0.mp4",title:"三日三餐减肥食谱",span:"百姓健康网"},
+                    {vid:4,img:"./img/video-1.png",src:"video-1.mp4",title:"四日三餐减肥食谱",span:"百姓健康网"},
+                    {vid:5,img:"./img/video-2.jpg",src:"video-0.mp4",title:"五日三餐减肥食谱",span:"百姓健康网"}
+                ]
+            }
+        },
+        methods:{
+            play(src){
+                this.src=src;
+            }
+        }
+    }
+</script>
+
+<style>
+    h1,p,ul,li{
+        margin:0;
+        padding:0;
+        list-style:none;
+    }
+    .vdo-title{
+        width:1210px;
+        height:80px;
+        margin:10px auto;
+        text-align:left;
+    }
+    .vdo-title>p{
+        color:#333;
+        font-size:28px;
+        font-family: "Microsoft YaHei";
+    }
+    .vdo-div{
+        display:flex;
+        justify-content:space-around;
+        width:1220px;
+        margin:0 auto;
+    }
+    #v1{
+        text-align:left;
+    }
+    .ul-img{
+        display:flex;
+        justify-content:space-around;
+        flex-wrap:wrap;
+        width:405px;
+        height:312px;
+        border:3px solid deeppink;
+        background-color:deeppink;
+        padding-top:1px;
+    }
+    .ul-img>li{
+        width:200px;
+        height:154px;
+        overflow:hidden;
+        position:relative;
+    }
+    .ul-img img{
+        width:200px;
+        height:154px;
+        transition:0.2s;
+    }
+    .ul-img span{
+        position:absolute;
+        left:0;
+        top:125px;
+        width:30px;
+        height:30px;
+        text-align:center;
+        line-height:30px;
+        color:#fff;
+        font-size:20px;
+        background-color:rgba(61, 59, 59,0.6);
+    }
+    .ul-img img:hover{
+        transform:scale(1.5);
+    }
+    .vdo-vdo{
+        width:750px;
+        display:flex;
+        height:320px;
+        align-items:center;
+        background-color:#66666E;
+        padding-left:20px;
+        position:relative;
+    }
+    .vdo-vdo>span{
+        position:absolute;
+        top:20px;
+        left:540px;
+        color:ghostwhite;
+    }
+    .vdo-li{
+        display:flex;
+        width:219px;
+        height:55px;
+        padding-left:24px;
+        padding-top:13px;
+        padding-bottom:13px;
+        position:relative;
+    }
+    .vdo-img{
+        width:96px;
+        height:54px;
+    }
+    .vdo-img>img{
+        width:96px;
+        height:54px;
+    }
+    .vdo-li>div>img{
+        width:30px;
+        height:30px;
+        opacity:0.9;
+        position:absolute;
+        left:60px;
+        top:25px;
+    }
+    .vdo-ul{
+        margin-top:25px;
+        height:255px;
+        text-align:left;
+        color:ghostwhite;
+        cursor:pointer;
+        overflow-x: hidden;
+        overflow-y: auto;
+    }
+    /* 滚动条样式 */
+    .vdo-ul::-webkit-scrollbar{
+        width: 6px;     /*高宽分别对应横竖滚动条的尺寸*/
+        height: 4px;
+    }
+    .vdo-ul::-webkit-scrollbar-thumb {/*滚动条里面小方块*/
+        border-radius: 5px;
+        -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+        background: rgba(0,0,0,0.2);
+    }
+    .vdo-ul::-webkit-scrollbar-track {/*滚动条里面轨道*/
+        -webkit-box-shadow: inset 0 0 5px rgba(0,0,0,0.2);
+        border-radius: 0;
+        background: rgba(0,0,0,0.1);
+    }
+    .vdo-ul>span{
+        position:absolute;
+        top:0px;
+        left:20px;
+    }
+    .vdo-p-img{
+        display:flex;
+        align-items:center;
+        padding-left:15px;
+        height:20px;
+        color:#A1A1A3;
+    }
+    .vdo-p-img p{
+        width:18px;
+        height:18px;
+        background:url("../../assets/video-sm.png") no-repeat center;
+        background-size:contain;
+        border-radius:50%;
+    }
+    .vdo-p-div{
+        width:120px;
+        font-size:12px;
+    }
+    .vdo-p-div>p{
+        padding-left:15px;
+        height:35px;
+    }
+
+</style>
---
+++ src/components/commou/Login.vue
@@ -0,0 +1,259 @@
+<template>
+    <div class="beijing">
+        <!-- 登陆 -->
+         <div class="kapian" v-show="pm==true">
+            <div class="ka1">
+                <a @click="ma" href="javascript:;"><img src="../../assets/login-1.jpg" alt=""></a>
+                <a @click="xin" href="javascript:;">新用户注册</a>
+            </div>
+            <div class="ka2">
+                <div>
+                账户:<input v-model="uname"  type="text" name="uname" placeholder="请输入用户名">
+                </div>
+                <span v-show="yin==true">用户名3~12位</span>
+            </div>
+            <div class="ka3">
+                <div>
+                密码:<input v-model="upwd"  type="password" name="upwd" placeholder="请输入密码"> 
+                </div>
+                  <span v-show="cang==true">密码3~8位</span>
+            </div>
+            <div class="ka4">
+                <div>
+                <input type="checkbox">自动登陆
+                </div>
+                <a href="javascript:;">忘记密码</a>
+            </div>
+            <div class="ka5">
+                <button @click="login">登陆</button>
+            </div>
+        </div>
+        <!-- 二维码 -->
+         <div class="ma" v-show="am==true">
+            <img src="../../assets/login-1.jpg" alt="">
+            <a @click="hui" href="javascript:;">账号登陆</a>
+        </div>
+        <!-- 注册 -->
+        <div class="zhu" v-show="cm==true">
+            <div class="zhu1">
+                <a @click="hui" href="javascript:;">登陆</a>
+                <div></div>
+            </div>
+            <div class="zhu2">
+                <div>用户名:</div>
+                <div>
+                <input type="text" name="uname" v-model="uname">
+                </div>
+                <span>用户名长度3~12位</span>
+            </div>
+            <div class="zhu3">
+                <div>登录密码: <input type="password" name="upwd" v-model="upwd"></div>
+                 <span>密码长度3~8位</span>
+            </div>
+            <div class="zhu4">
+                <div>确认密码: <input type="password" name="upwd" v-model="upwd"></div>
+                 <span>密码长度3~8位</span>
+            </div>
+            <div class="zhu5">
+                <button>提交用户注册信息</button>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            yin:false,
+            cang:false,
+            uname:"",
+            upwd:"" ,
+            pm:true,
+            am:false,
+            cm:false
+        }
+    },
+    methods:{
+     login(){
+         var n=this.uname;
+         var p=this.upwd;
+         var nreg=/^[a-zA-Z0-9]{3,10}$/;
+         var preg=/^[a-zA-Z0-9]{3,8}$/;
+            if(this.uname==""&&!nreg.test(n)){
+                this.yin=true;
+            }else{
+                this.yin=false;
+            }
+            if(this.upwd==""&&!preg.test(p)){
+                this.cang=true;
+            }else{
+                this.cang=false;
+            }
+            this.$router.push("/")
+        },
+        ma(){
+            this.pm=false;
+            this.am=true;
+            this.cm=false;
+        },
+        hui(){
+            this.pm=true;
+            this.cm=false;
+            this.am=false;
+        },
+        xin(){
+            this.pm=false;
+            this.cm=true;
+            this.am=false; 
+        }
+    },
+    wacth(){
+        this.login();
+    }
+}
+</script>
+<style scoped>
+/*背景图片*/
+.beijing{
+    width: 1200px;
+    height: 600px;
+    background-image: url(../../assets/login-2.jpg);
+    background-repeat: no-repeat;
+    background-size: 1200px 600px;
+    margin: 0 auto;
+    position: relative;
+}
+/*登陆*/
+.ma>img{
+    width: 200px;
+    height: 200px;
+    position: absolute;
+    top:200px;
+    left: 150px;
+}
+.ma>a{
+  position: relative;
+  left: -280px;
+  top: 150px;
+  text-decoration: none;
+  color: #fff
+}
+.kapian{
+    width: 250px;
+    height: 200px;
+    line-height: 40px;
+	background-color:rgba(0,0,0,0);
+	padding:5px;
+    position: absolute;
+    top:120px;
+    left:100px;
+}
+/*ka1*/
+.ka1{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color: #fff;
+}
+.ka1>a{
+    text-decoration: none;
+    color: #fff;
+}
+.ka1>a>img{
+    width: 40px;
+    height: 40px;
+}
+/*ka2,ka3,ka4*/
+.ka2>div>input,.ka3>div>input{
+    width:210px;height:38px;
+	border:0;outline:0;
+    background:#fff url(../../assets/login-3.jpg) no-repeat 97% center;
+	font-size:13.333px;
+	box-sizing:border-box;
+	padding:10px 0;
+	margin:18px 0;
+	padding-left:15px;
+
+}
+.ka3>div>input{
+    background-image:url(../../assets/login-4.jpg);		
+}
+.ka2,.ka3,.ka4{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color:#fff;
+    font-weight: 700;
+}
+.ka2{
+    width: 340px;
+}
+.ka3{
+    width: 330px;
+}
+.ka2>span,.ka3>span{
+    font-size: 10px;
+    color: red;
+}
+/*ka4*/
+.ka4>a{
+    text-decoration: none;
+    color: #fff;
+}
+/*ka5*/
+.ka5>button{
+    width: 250px;
+    height: 35px;
+    background-color:#0aa1ed;
+    border:0;
+    font-size: 18px;
+    color: #fff;
+}
+/*注册*/
+.zhu{
+    width: 440px;
+    height: 400px;
+    line-height: 50px;
+	background-color:rgba(0,0,0,0);
+	padding:5px;
+    position: absolute;
+    top:120px;
+    left:100px;
+}
+.zhu1,.zhu2,.zhu3,.zhu4{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    color:#fff;
+    font-weight: 700;
+}
+.zhu2>div>input,.zhu3>div>input,.zhu4>div>input{
+    width:210px;height:30px;
+	border:0;outline:0;
+	font-size:13.333px;
+	box-sizing:border-box;
+	padding:10px 0;
+	margin:18px 0;
+	padding-left:15px;
+}
+.zhu2>span,.zhu3>span,.zhu4>span{
+    color: red;
+}
+.zhu1>a>img{
+    width: 40px;
+    height: 40px;
+}
+.zhu1>a{
+    text-decoration: none;
+    color: #fff;
+}
+.zhu5>button{
+    width: 210px;
+    height: 35px;
+    background-color:#0aa1ed;
+    border:0;
+    font-size: 18px;
+    color: #fff;
+    margin-right: 80px;
+}
+</style>
